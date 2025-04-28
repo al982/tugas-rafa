f38139005b7d14bdef9208277e6e1481d635e61d
al982 <aalghazali984@gmail.com>
2025-04-28 08:55:37 +0800

    Add files via upload

---
+++ tugas5.php
@@ -0,0 +1,10 @@
+<?php
+$nilai_matematika = 85;
+$nilai_bahasa = 75;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >=70) {
+    echo"Selamat! Kamu boleh nonton bioskop akhir pekan ini!";
+} else {
+    echo "Yah... belajar lagi ya, lain kali bisa!";
+}
+?>
