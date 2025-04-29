0eaffdfef1daf658779acb17c57ce8c5737e19c9
al982 <aalghazali984@gmail.com>
2025-04-29 14:06:46 +0800

    Add files via upload

---
+++ tugas7.php
@@ -0,0 +1,8 @@
+<?php
+$bukhari="sehat";
+$motor="mogok";
+if ($bukhari =="sehat" && $motor =="menyala" ) {
+    echo"berangkat ke sekolah";
+}else {
+    echo"tidak berangkat ke sekolah";
+}
---
+++ tugas8.php
@@ -0,0 +1,7 @@
+<?php
+$azzam_puasa= false ;
+if(!$azzam_puasa ) {
+    echo"azzam makan nasi padang di siang hari";
+} else {
+    echo"berbuka dengan nasi padang";
+}
